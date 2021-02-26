04236fc729706abdd569d73fe8cd28445530082b
Denis Zemskikh <denis@zemskix.com>
2021-02-26 14:08:27 +0300

    end course

--- Hello/ViewController.swift
+++ Hello/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var lbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func btn(_ sender: Any) {
+        lbl.text = "It worked!"
+    }
+    
 }
 
